f0e9452cd0793623bf8ed816b86e544a1b8077bc
BLADEAPEX <tmyers1396@gmail.com>
2015-12-09 15:45:31 -0500

    Pseudocode and commenting
    update current phrase

--- main/Functions/update_phrase.cpp
+++ main/Functions/update_phrase.cpp
@@ -1,32 +1,24 @@
 /**************************************************************************
- *Function Name      :    insertFile
- *Author             :    Loren Davies
- *Date               :    11/20/2015
+ *Function Name      :    update_phrase
+ *Author             :    Tyler Myers
+ *Date               :    12/9/2015
  *Course/Section     :    CSC 263 - 001
- *Program Description:    Reads line from txt file, and adds it to
- * structure. Then call function to add it to array.
+ *Program Description:    Changes the current phrase
  *
- *BEGIN insertFile
- * 	Input file name
- *	Open file
- *	IF there's a problem opening a file
- *		Output error message
- *	ELSE file did open properly
- *		Read first line, but do nothing with it
- *		WHILE it isn't the end of the file
- *			Input Game_Number add to game
- *			Input Date add to game
- *			Input Opponent add to game
- *			Input Home_Away add to game
- *			IF Home_Away doesn't equal A or H
- *				Add Home_Away to Opponent
- *				Input Home_Away add to game
+ *BEGIN update_phrase
+ *	FOR(i less then game phrase count)
+ *		IF(index of current phrase is not equal to a space)
+ *			add index character to temp
+ *		ELSE IF(index of current phrase is equal to a space)
+ *			IF(index of game phrase is equal to guessed letter
+ *				add index character to temp
+ *			else
+ *				add a space to temp
  *			END IF
- *			Input Score add to game
- *			Input Their_Score add to game
- *			Input Result add to game
- *			Parse and add Game_Number to Game
- *		END WHILE
+ *		else
+ *			add a space to temp
+ *		END IF
+ *	END FOR
  *END insertFile
  **************************************************************************/
  
@@ -38,33 +30,33 @@
 string update_phrase(string Game_Phrase, string Guessed_Letter, string Current_Phrase)
 {
 	//Global Constants
-	const string Space = " ";			//stores a space
+	const string SPACE = " ";			//stores a space
 	
 	//Global Variables
 	string Temp = "";					//Holds the phrase as it updates
 	
 	for(int i = 0; i < Game_Phrase.length(); i++)//counter for checking all characters
 	{
-		if(Current_Phrase.substr(i,1) != space)//If index is not equal to a space
+		if(Current_Phrase.substr(i,1) != SPACE)//If index is not equal to a space
 		{
-			temp += Current_Phrase.substr(i,1);//Add that index character to temp
+			Temp += Current_Phrase.substr(i,1);//Add that index character to temp
 		}
-		else if(Current_Phrase.substr(i,1) == space)//If index is equal to a space
+		else if(Current_Phrase.substr(i,1) == SPACE)//If index is equal to a space
 		{
 			if(Game_Phrase.substr(i,1) == Guessed_Letter)//If index is equal to guessed letter
 			{
-				temp += Game_Phrase.substr(i,1);//Add that index character to temp
+				Temp += Game_Phrase.substr(i,1);//Add that index character to temp
 			}
 			else//If it's not eaual to the guessed letter
 			{
-				temp += space;//Add that index character to temp
+				Temp += SPACE;//Add that index character to temp
 			} 
 		}
 		else
 		{
-			temp += " ";
+			Temp += " ";
 		}
 	}
-	return temp;
+	return Temp;
 	
 }//End update_phrase
